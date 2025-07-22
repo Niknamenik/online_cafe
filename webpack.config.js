d1195c630834414a2dd70fa17acbbfe240d64430
Niknamenik <nikita20nik00@gmail.com>
2025-07-22 19:21:47 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "docs"),
     filename: "bundle.js",
-    publicPath: "/online_cafe/",
+    publicPath: "./",
   },
   plugins: [
     new MiniCssExtractPlugin(),
